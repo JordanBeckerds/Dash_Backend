2e46bb472e584e79bee019ad3c938b9b0f56bba9
Jordan Becker <134543576+JordanBeckerds@users.noreply.github.com>
2024-03-08 21:21:02 +0400

    Delete SQL DB/SQL DB directory

--- SQL DB/SQL DB/ALTER/%+File.sql
+++
@@ -1,5 +0,0 @@
-ALTER TABLE ClientProject
-ADD ProjectCompletion TINYINT UNSIGNED DEFAULT 0 CHECK (ProjectCompletion >= 0 AND ProjectCompletion <= 100);
-
-ALTER TABLE ClientProject
-ADD ProjectFiles BLOB;
